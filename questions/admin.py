5c25b5c27d0f7358b773bbc709e07e25b9cf418f
Bernhard Glück <business@bernhardglueck.com>
2022-12-28 02:38:54 +0100

    Added features requested.

--- questions/admin.py
+++ questions/admin.py
@@ -3,7 +3,7 @@ from django.contrib.admin import ModelAdmin
 from django.contrib.admin.options import InlineModelAdmin, TabularInline
 from django.http import HttpResponseRedirect
 
-from questions.models import Question, Answer, Tag
+from questions.models import Question, Answer, Tag, Fact
 
 
 class AnswerAdmin(TabularInline):
@@ -16,19 +16,46 @@ class TagAdmin(ModelAdmin):
     exclude = ('text',)
 
 
+class FactAdmin(ModelAdmin):
+    list_display = ('slug', 'tag_text')
+    exclude = ('slug',)
+
+
 class QuestionAdmin(ModelAdmin):
-    list_display = ('slug', 'tag_text', 'created_by', 'verified_by')
+    list_display = ('slug', 'tag_text', 'created_by', 'verified_by', 'is_active')
     inlines = [AnswerAdmin]
     readonly_fields = ('created_by', 'verified_by')
     exclude = ('text', 'slug')
 
+    def change_view(self, request, object_id, form_url="", extra_context=None):
+        extra_context = extra_context or {}
+        extra_context['can_verify'] = False
+        extra_context['can_unverify'] = False
+
+        if object_id:
+            question = Question.objects.get(pk=object_id)
+            extra_context['can_verify'] = question.can_verify(request)
+            extra_context['can_unverify'] = question.verified_by is not None
+
+        return super(QuestionAdmin, self).change_view(
+            request, object_id, form_url, extra_context=extra_context,
+        )
+
     def response_change(self, request, obj: Question):
         if "_verify" in request.POST:
 
-            if obj.created_by:
-                obj.verified_by = request.user
-                obj.save()
+            if obj.can_verify(request):
+                obj.verify(request)
                 self.message_user(request, "This question has been verified")
+            else:
+                self.message_user(request, "Cannot verify question")
+
+            return HttpResponseRedirect(".")
+
+        if "_unverify" in request.POST:
+            if obj.verified_by:
+                obj.verified_by = None
+                obj.save()
 
             return HttpResponseRedirect(".")
 
@@ -39,13 +66,11 @@ class QuestionAdmin(ModelAdmin):
         if not change:
             obj.created_by = request.user
 
-            if "_verify" in request.POST:
-                obj.verified_by = request.user
-
         super().save_model(request, obj, form, change)
 
 
 admin.site.register(Tag, TagAdmin)
 admin.site.register(Question, QuestionAdmin)
+admin.site.register(Fact, FactAdmin)
 
 admin.site.site_header = 'Quiz questions'
--- questions/models.py
+++ questions/models.py
@@ -2,6 +2,7 @@ from typing import List
 
 from django.db import models
 from django.contrib.auth.models import User
+from django.http import HttpRequest
 from django.utils.text import slugify
 
 
@@ -17,27 +18,16 @@ class Tag(models.Model):
         return self.text
 
 
-class Question(models.Model):
+class TaggedItem(models.Model):
+    class Meta:
+        abstract = True
+
     text = models.TextField()
     slug = models.SlugField()
 
     tags = models.ManyToManyField(Tag)
 
     source = models.URLField(null=True, blank=True)
-
-    created_by = models.ForeignKey(User,
-                                   related_name='created_by',
-                                   on_delete=models.PROTECT,
-                                   editable=True,
-                                   null=False,
-                                   blank=False)
-
-    verified_by = models.ForeignKey(User,
-                                    related_name="verified_by",
-                                    on_delete=models.SET_NULL,
-                                    editable=True,
-                                    null=True,
-                                    blank=True)
 
     def tag_text(self):
         tag_objects = self.tags.order_by('level')
@@ -57,10 +47,45 @@ class Question(models.Model):
     def save(self, *args, **kwargs):
         self.slug = slugify(self.get_title())
 
-        return super(Question, self).save(*args, **kwargs)
+        return super(TaggedItem, self).save(*args, **kwargs)
 
     def __str__(self):
         return self.get_title()
+
+
+class Fact(TaggedItem):
+    pass
+
+
+class Question(TaggedItem):
+    is_active = models.BooleanField(default=True)
+
+    created_by = models.ForeignKey(User,
+                                   related_name='created_by',
+                                   on_delete=models.PROTECT,
+                                   editable=True,
+                                   null=False,
+                                   blank=False)
+
+    verified_by = models.ForeignKey(User,
+                                    related_name="verified_by",
+                                    on_delete=models.SET_NULL,
+                                    editable=True,
+                                    null=True,
+                                    blank=True)
+
+    def can_verify(self, request: HttpRequest) -> bool:
+        if hasattr(self, 'created_by') and self.created_by:
+            if hasattr(self, "verified_by") and not self.verified_by:
+                if request.user != self.created_by:
+                    return True
+
+        return False
+
+    def verify(self, request: HttpRequest) -> bool:
+        if self.can_verify(request):
+            self.verified_by = request.user
+            self.save()
 
 
 class Answer(models.Model):
